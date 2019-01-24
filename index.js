2cf4c4a955c54ef716f61bb08a49e9ce3c7aa1f3
Leslie Jones-Dove <ljonesdove@cs.uchicago.edu>
2019-01-24 11:53:02 -0600

    server update

--- index.js
+++ index.js
@@ -14,12 +14,23 @@ app.use(bodyParser.json())
  
 
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+console.log(process.env)
 const port = process.env.PORT || 8080
 
 var server = app.listen(port, () => {
 	console.log('Now listening to port ' + port);
 })
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '/public')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+  });
+}
 
 const routes = require('./routes');
 app.use('/api', routes)
